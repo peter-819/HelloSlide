c6e34c1c79e1359785b751fc38bd3ab923a81045
peter-819 <peter-819@outlook.com>
2020-02-12 22:37:42 +0800

    2020.2.12 顶部导航栏测试

---
+++ src/components/DrawingStage.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-stage :config="configKonva">
+    <v-layer>
+      <v-circle :config="configCircle"></v-circle>
+    </v-layer>
+  </v-stage>
+</template>
+
+<script>
+  export default{
+    name: "DrawingStage",
+    data: () => ({
+      configKonva:{
+        width:window.innerWidth,
+        height:window.innerHeight
+      },
+      configCircle:{
+        x:100,
+        y:100,
+        radius:70,
+        fill:"red",
+        stroke:"black",
+        strokeWidth:4,
+        draggable:true
+      }
+    })
+  }
+</script>
---
+++ src/components/StagePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <DrawingStage></DrawingStage>
+  </div>
+</template>
+
+<script>
+
+import DrawingStage from '@/components/DrawingStage'
+
+export default {
+  name: "StagePage",
+  components:{
+    DrawingStage
+  },
+  data: () =>({
+
+  })
+}
+</script>
